b1f0fd09147d8514e12d3fd0e56bd51b955df680
kpatrickbarrett <kpatrickbarrett@gmail.com>
2015-01-30 09:26:38 -0500

    added hyphen
    chance for hyphenated name

--- namegen.py
+++ namegen.py
@@ -7,7 +7,13 @@ lname = open("lnames.txt", "r")
 #first = fname.readline(random.randint(0,5164))
 #last = lname.readline(random.randint(0,88800))
 
+hyphenCheck = random.randint(0,100)
 first =  linecache.getline('lnames.txt', (random.randint(0,93963)))
 last =  linecache.getline('lnames.txt', (random.randint(0,94000)))
+lastHyphen =  linecache.getline('lnames.txt', (random.randint(0,94000)))
 
-print((first.rstrip())+ " " + last)
+if hyphenCheck > 75:
+	print((first.rstrip())+" "+(last.rstrip())+"-"+lastHyphen)
+
+else:
+	print((first.rstrip())+ " " + last)
